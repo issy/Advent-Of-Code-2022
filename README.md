7ddcaf18a7294423b688fd4540d38cb72a844dac
issy <issy@issy.dev>
2022-11-30 01:30:01 +0000

    Change project structure to allow for more frameworks

--- README.md
+++ README.md
@@ -4,18 +4,6 @@
 
 This is just a little repo to hold my solutions for the Advent Of Code challenge for 2022
 
-To get started, create a python venv (make sure you have at least version 3.10), install the project requirements and run `python3 scripts/new_day.py`
+To get started, run `python3 scripts/new_day.py <framework>` passing in a valid framework parameter
 
 From there, you can fill in your sample input/outputs and your actual problem input in the directory `src/day_X/` where X is the day of the month.
-
-## Testing
-
-This repo is set up with a test pipeline, so pytest will automatically run tests for each day based on sample input/output
-
-You can run this manually with `python3 -m pytest`
-
-## Developing
-
-Include your solutions in `main.py` in the day directory. Feel free to add extra files if you need to provide abstractions for certain solutions. Calling part 1 from part 2 of a challenge is fine, too.
-
-Run your code with `python3 src/day_X/main.py`
--- scripts/new_day.py
+++ scripts/new_day.py
@@ -4,40 +4,54 @@ from datetime import datetime
 import shutil
 
 
-def raise_if_exists(filepath: str):
-    if os.path.exists(filepath):
-        raise RuntimeError(f"{filepath} already exists")
-
-
-def touch_file(filepath: str):
-    with open(filepath, "w"):
-        pass
+frameworks = {
+    "python": {
+        "base_path": "python",
+        "files": (
+            ("__init__.py", None),
+            ("main.py", "main.py.template"),
+            ("input.txt", None),
+            ("sample_input.txt", None),
+            ("sample_output_1.txt", None),
+            ("sample_output_2.txt", None),
+            ("test.py", "test.py.template"),
+        )
+    },
+    "typescript": {
+        "base_path": "typescript",
+        "files": (
+            ("index.ts", "index.ts.template"),
+            ("index.ts.spec", "index.ts.spec.template")
+        )
+    }
+}
 
 
 def main():
-    day_number = int(sys.argv[1]) if len(sys.argv) == 2 else datetime.now().date().day
-    day_directory = f"./src/day_{day_number}"
+    if len(sys.argv) < 1:
+        raise RuntimeError("Missing required argument 'framework'")
+
+    framework = sys.argv[1]
+    if framework not in frameworks:
+        raise RuntimeError(f"Invalid framework {framework!r} - valid frameworks are {list(frameworks)}")
+
+    day_number = int(sys.argv[2]) if len(sys.argv) == 3 else datetime.now().date().day
+    base_path = frameworks[framework]["base_path"]
+    day_directory = f"./src/{base_path}/day_{day_number}"
     if os.path.exists(day_directory):
         raise RuntimeError(f"Directory for day {day_number} already exists")
 
     os.mkdir(day_directory)
 
-    for filename, template_name in (
-        ("__init__.py", None),
-        ("main.py", "main.py.template"),
-        ("input.txt", None),
-        ("sample_input.txt", None),
-        ("sample_output_1.txt", None),
-        ("sample_output_2.txt", None),
-        ("test.py", "test.py.template"),
-    ):
+    for filename, template_name in frameworks[framework]["files"]:
         filepath = f"{day_directory}/{filename}"
         if template_name is None:
-            touch_file(filepath)
+            with open(filepath, "w"):
+                pass
         else:
             shutil.copyfile(f"./templates/{template_name}", filepath)
 
-    print(f"Initialised day directory for day {day_number}")
+    print(f"Initialised {framework} directory for day {day_number}")
 
 
 if __name__ == "__main__":
