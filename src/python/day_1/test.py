1fc835b3676b2ff008664809282fb64f30101193
issy <issy@issy.dev>
2022-12-02 18:08:33 +0000

    Adjust test template to work for multiple days

--- src/python/day_1/test.py
+++ src/python/day_1/test.py
@@ -3,14 +3,11 @@ import os.path
 from .main import part_1, part_2
 
 
-os.chdir(os.path.dirname(__file__))
-
-
 def test_part_1():
-    with open(f"../../../common/{os.path.split(os.getcwd())[-1]}/sample_input.txt", "r") as fp:
+    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_input.txt", "r") as fp:
         sample_input = fp.read()
 
-    with open(f"../../../common/{os.path.split(os.getcwd())[-1]}/sample_output_1.txt", "r") as fp:
+    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_output_1.txt", "r") as fp:
         expected_output = fp.read()
 
     returned_output = part_1(sample_input)
@@ -18,10 +15,10 @@ def test_part_1():
 
 
 def test_part_2():
-    with open(f"../../../common/{os.path.split(os.getcwd())[-1]}/sample_input.txt", "r") as fp:
+    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_input.txt", "r") as fp:
         sample_input = fp.read()
 
-    with open(f"../../../common/{os.path.split(os.getcwd())[-1]}/sample_output_2.txt", "r") as fp:
+    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_output_2.txt", "r") as fp:
         expected_output = fp.read()
 
     returned_output = part_2(sample_input)
