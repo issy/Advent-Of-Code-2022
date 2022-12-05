af8f788c0e0082d79a96f2c9e46ed63d2462b1ec
issy <issy@issy.dev>
2022-12-05 09:20:11 +0000

    Add day 5 solution in python

---
+++ src/python/day_5/test.py
@@ -0,0 +1,25 @@
+import os.path
+
+from .main import part_1, part_2
+
+
+def test_part_1():
+    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_input.txt", "r") as fp:
+        sample_input = fp.read()
+
+    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_output_1.txt", "r") as fp:
+        expected_output = fp.read()
+
+    returned_output = part_1(sample_input)
+    assert returned_output == expected_output
+
+
+def test_part_2():
+    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_input.txt", "r") as fp:
+        sample_input = fp.read()
+
+    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_output_2.txt", "r") as fp:
+        expected_output = fp.read()
+
+    returned_output = part_2(sample_input)
+    assert returned_output == expected_output
