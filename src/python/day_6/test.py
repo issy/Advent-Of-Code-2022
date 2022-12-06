cf50aaaf43170e0a2a888f02c605fdeca020162f
issy <issy@issy.dev>
2022-12-06 11:50:18 +0000

    Update tests

--- src/python/day_6/test.py
+++ src/python/day_6/test.py
@@ -1,25 +1,28 @@
 import os.path
 
+import pytest
+
 from .main import part_1, part_2
 
 
-def test_part_1():
-    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_input.txt", "r") as fp:
+def get_sample_input_output(input_filename: str, output_filename: str) -> list[tuple[str, int]]:
+    base_dir = f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}"
+    with open(f"{base_dir}/{input_filename}", "r") as fp:
         sample_input = fp.read()
 
-    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_output_1.txt", "r") as fp:
+    with open(f"{base_dir}/{output_filename}", "r") as fp:
         expected_output = fp.read()
 
-    returned_output = part_1(sample_input)
-    assert str(returned_output) == expected_output
+    input_lines = sample_input.splitlines()
+    output_lines = expected_output.splitlines()
+    return [(input_line, int(output_lines[line_num])) for line_num, input_line in enumerate(input_lines)]
 
 
-def test_part_2():
-    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_input.txt", "r") as fp:
-        sample_input = fp.read()
+@pytest.mark.parametrize("sample_input,expected_output", get_sample_input_output("sample_input_1.txt", "sample_output_1.txt"))
+def test_part_1(sample_input: str, expected_output: int):
+    assert part_1(sample_input) == expected_output
 
-    with open(f"../../common/{os.path.split(os.path.split(__file__)[0])[-1]}/sample_output_2.txt", "r") as fp:
-        expected_output = fp.read()
 
-    returned_output = part_2(sample_input)
-    assert str(returned_output) == expected_output
+@pytest.mark.parametrize("sample_input,expected_output", get_sample_input_output("sample_input_2.txt", "sample_output_2.txt"))
+def test_part_2(sample_input: str, expected_output: int):
+    assert part_2(sample_input) == expected_output
